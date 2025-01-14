c9926596f703a478c96a8456779fa72c6c4c5b4c
georgeragan <georgeragan123@gmail.com>
2025-01-14 13:48:21 +0530

    first_commit

---
+++ main.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI
+from fastapi .middleware.cors import CORSMiddleware
+import pickle
+import json
+from pydantic import BaseModel
+
+app=FastAPI()
+
+origins=["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+class model_input(BaseModel):
+    age:int
+    sex:int
+    bmi:float
+    children:int
+    smoker:int
+    region: int
+
+model=pickle.load(open("insurance_model","rb"))
+
+@app.post("/insurance_prediction")#When a client sends a POST request to this URL with the required input data, the prediction function is executed.
+
+def prediction(input_parameters:model_input):
+    input_dict=input_parameters.dict()
+    age=input_dict["age"]    
+    sex=input_dict["sex"]
+    bmi=input_dict["bmi"]
+    children=input_dict["children"]
+    smoker=input_dict["smoker"]
+    region=input_dict["region"]
+    predictions=model.predict([[age,sex,bmi,children,smoker,region]])
+    return {"prediction":predictions}
